34d8eaa4c8a94c47701ab597634b17620b625d1a
Tim Luginbuehl <tl@oxon.ch>
2017-07-10 17:51:26 +0200

    colors and small layouting issues fixed.

--- blockly/blocks/arduino/oxocard.js
+++ blockly/blocks/arduino/oxocard.js
@@ -113,18 +113,17 @@ Blockly.Blocks['oxocard_println'] = {
 /* ---------- Display ---------- */
 Blockly.Blocks['oxocard_turn_display_on'] = {
 	helpUrl: 'http://www.oxocard.ch/oxocard-befehle/',
-	init: createGenericOxocardInitFunc('Turn display on', 'Turn the whole display on')
+	init: createGenericOxocardInitFunc('Turn display on', 'Turn the whole display on', Blockly.Blocks.oxocard.COLOUR_METHOD)
 };
 
 Blockly.Blocks['oxocard_clear_display'] = {
 	helpUrl: 'http://www.oxocard.ch/oxocard-befehle/',
-	init: createGenericOxocardInitFunc('Clear display', 'Turn the whole display off')
+	init: createGenericOxocardInitFunc('Clear display', 'Turn the whole display off', Blockly.Blocks.oxocard.COLOUR_METHOD)
 };
 
 Blockly.Blocks['oxocard_fill_display'] = {
   helpUrl: 'http://www.oxocard.ch/oxocard-befehle/',
   init: function() {
-    this.setColour(230);
 	this.appendDummyInput()
         .appendField("Fill display");
     this.appendValueInput("BRIGHTNESS", 'Brightness')
@@ -133,13 +132,13 @@ Blockly.Blocks['oxocard_fill_display'] = {
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setTooltip('Write value between 0 and 255');
+    this.setColour(Blockly.Blocks.oxocard.COLOUR_METHOD);
   }
 };
 
 Blockly.Blocks['oxocard_draw_image'] = {
 	helpUrl: 'http://www.oxocard.ch/oxocard-befehle/',
 	init: function() {
-		this.setColour(15);
 		this.appendDummyInput()
 			.appendField("Draw image");
 		for(var i=0, l=8; i<l; i++){
@@ -154,13 +153,13 @@ Blockly.Blocks['oxocard_draw_image'] = {
 		this.setPreviousStatement(true, null);
 		this.setNextStatement(true, null);
 		this.setTooltip('Turn on specific pixels');
+		this.setColour(Blockly.Blocks.oxocard.COLOUR_METHOD);
 	}
 };
 
 Blockly.Blocks['oxocard_draw_pixel'] = {
 	helpUrl: 'http://www.oxocard.ch/oxocard-befehle/',
 	init: function() {
-		this.setColour(15);
 		this.appendDummyInput()
 			.appendField("Draw pixel on X:")
 			.appendField(new Blockly.FieldDropdown([["1", 0], ["2", 1], ["3", 2], ["4", 3], ["5", 4], ["6", 5], ["7", 6], ["8", 7]]), "X")
@@ -173,6 +172,7 @@ Blockly.Blocks['oxocard_draw_pixel'] = {
 		this.setPreviousStatement(true, null);
 		this.setNextStatement(true, null);
 		this.setTooltip('Turn on specific pixels');
+		this.setColour(Blockly.Blocks.oxocard.COLOUR_METHOD);
 	}
 };
 
@@ -180,7 +180,6 @@ Blockly.Blocks['oxocard_draw_line'] = {
 	helpUrl: 'http://www.oxocard.ch/oxocard-befehle/',
 	init: function() {
 		var matrix_list = [["1", "0"], ["2", "1"], ["3", "2"], ["4", "3"], ["5", "4"], ["6", "5"], ["7", "6"], ["8", "7"]];
-		this.setColour(15);
 		this.appendDummyInput()
 			.appendField("Draw line")
 		this.appendDummyInput()
@@ -200,18 +199,19 @@ Blockly.Blocks['oxocard_draw_line'] = {
 		this.setPreviousStatement(true, null);
 		this.setNextStatement(true, null);
 		this.setTooltip('Draw a line');
+		this.setColour(Blockly.Blocks.oxocard.COLOUR_METHOD);
 	}
 };
 
 Blockly.Blocks['oxocard_smile'] = {
 	helpUrl: 'http://www.oxocard.ch/oxocard-befehle/',
-	init: createGenericOxocardInitFunc('Draw smile', 'A simple smile :)')
+	init: createGenericOxocardInitFunc('Draw smile', 'A simple smile :)', Blockly.Blocks.oxocard.COLOUR_METHOD)
 };
 
 
 Blockly.Blocks['oxocard_wink'] = {
 	helpUrl: 'http://www.oxocard.ch/oxocard-befehle/',
-	init: createGenericOxocardInitFunc('Draw wink', 'A simple wink ;)')
+	init: createGenericOxocardInitFunc('Draw wink', 'A simple wink ;)', Blockly.Blocks.oxocard.COLOUR_METHOD)
 };
 
 
@@ -219,35 +219,35 @@ Blockly.Blocks['oxocard_wink'] = {
 Blockly.Blocks['oxocard_get_acceleration'] = {
   helpUrl: 'http://www.oxocard.ch/oxocard-befehle/',
   init: function() {
-    this.setColour(10);
     this.appendDummyInput()
         .appendField("Get acceleration")
         .appendField(new Blockly.FieldDropdown([["X", "X"], ["Y", "Y"], ["Z", "Z"]]), "AXIS")
     this.setOutput(true, 'Number');
     this.setTooltip('Return the selected accelerometer axis value');
+    this.setColour(Blockly.Blocks.oxocard.COLOUR_VARIABLE);
   }
 };
 
 Blockly.Blocks['oxocard_get_orientation'] = {
   helpUrl: 'http://www.oxocard.ch/oxocard-befehle/',
   init: function() {
-    this.setColour(10);
     this.appendDummyInput()
         .appendField("Get orientation")
     this.setOutput(true, 'Number');
     this.setTooltip('Return the orientation of the OXOcard');
+    this.setColour(Blockly.Blocks.oxocard.COLOUR_VARIABLE);
   }
 };
 
 Blockly.Blocks['oxocard_is_orientation'] = {
   helpUrl: 'http://www.oxocard.ch/oxocard-befehle/',
   init: function() {
-    this.setColour(10);
     this.appendDummyInput()
         .appendField("Is orientation")
         .appendField(new Blockly.FieldDropdown([["Up", "Up"], ["Down", "Down"], ["Horizontally", "Horizontally"], ["Vertically", "Vertically"]]), "DIRECTION")
     this.setOutput(true, 'Boolean');
     this.setTooltip('Return if the OXOcard is in the given orientation');
+    this.setColour(Blockly.Blocks.oxocard.COLOUR_VARIABLE);
   }
 };
 
@@ -256,7 +256,6 @@ Blockly.Blocks['oxocard_is_orientation'] = {
 Blockly.Blocks['oxocard_setup_as_ibeacon'] = {
   helpUrl: 'http://www.oxocard.ch/oxocard-befehle/',
   init: function() {
-    this.setColour(10);
     this.appendDummyInput()
         .appendField("Setup the OXOcard as iBeacon")
     this.appendValueInput("ID", 'Id')
@@ -265,13 +264,13 @@ Blockly.Blocks['oxocard_setup_as_ibeacon'] = {
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setTooltip('Sets the OXOcard up as iBeacon with the given ID');
+    this.setColour(Blockly.Blocks.oxocard.COLOUR_METHOD);
   }
 };
 
 Blockly.Blocks['oxocard_find_ibeacon'] = {
   helpUrl: 'http://www.oxocard.ch/oxocard-befehle/',
   init: function() {
-    this.setColour(10);
     this.appendDummyInput()
         .appendField("Find iBeacon")
     this.appendValueInput("ID", 'Id')
@@ -280,6 +279,7 @@ Blockly.Blocks['oxocard_find_ibeacon'] = {
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setTooltip('Serach for iBeacons with the given ID');
+    this.setColour(Blockly.Blocks.oxocard.COLOUR_METHOD);
   }
 };
 
@@ -288,7 +288,6 @@ Blockly.Blocks['oxocard_find_ibeacon'] = {
 Blockly.Blocks['oxocard_tone'] = {
   helpUrl: 'http://www.oxocard.ch/oxocard-befehle/',
   init: function() {
-    this.setColour(10);
     this.appendDummyInput()
         .appendField("Play tone")
     this.appendValueInput("FREQUENCY", 'Frequency')
@@ -300,10 +299,11 @@ Blockly.Blocks['oxocard_tone'] = {
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setTooltip('Plays a tone for the givern time');
+    this.setColour(Blockly.Blocks.oxocard.COLOUR_METHOD);
   }
 };
 
 Blockly.Blocks['oxocard_no_tone'] = {
 	helpUrl: 'http://www.oxocard.ch/oxocard-befehle/',
-	init: createGenericOxocardInitFunc('No tone', 'Stops the tone')
+	init: createGenericOxocardInitFunc('No tone', 'Stops the tone', Blockly.Blocks.oxocard.COLOUR_METHOD)
 };

2e166409ebb6e0b211f829a2434010ab6adefa0c
Tim Luginbuehl <tl@oxon.ch>
2017-07-20 13:55:27 +0200

    current state of frontend for webbased compiling

--- ardublockly/oxocard_uploader.js
+++ ardublockly/oxocard_uploader.js
@@ -1,65 +1,89 @@
 'use strict';
 
-var OxocardUploader = OxocardUploader || {};
-
-OxocardUploader.COMPILE_URL='http://localhost:1337/compile';
-OxocardUploader.UPLOAD_URL='https://localhost:8992/upload';
-OxocardUploader.FLASH_COMMAND={
-	"board": "arduino:avr:uno",
-	"port": "/dev/ttyUSB0",
-	"filename": "sketch_jul11a.hex",
-	"commandline": "\"{runtime.tools.avrdude.path}/bin/avrdude\" \"-C{runtime.tools.avrdude.path}/etc/avrdude.conf\" {upload.verbose}  -patmega328p -carduino -P{serial.port} -b57600 -V -D \"-Uflash:w:{build.path}/{build.project_name}.hex:i\"",
-	"extra": {
-		"auth": {
-			"password": null
-		},
-		"wait_for_upload_port": false,
-		"use_1200bps_touch": false,
-		"network": false,
-		"params_verbose": "-v",
-		"params_quiet": "-q -q",
-		"verbose": false
-	}
-};
-
-OxocardUploader.init = function() {
-	
-};
-
-OxocardUploader.compile = function(inoContent){
-	var http = new XMLHttpRequest();
-	http.open("POST", OxocardUploader.COMPILE_URL, true);
-
-	http.setRequestHeader("Content-type", "text/plain");
-
-	http.onreadystatechange = function() {
-		if(http.readyState == 4 && http.status == 200) {
-			var response = JSON.parse(http.responseText);
-			console.log(response);
-
-			OxocardUploader.upload(response['hex']);
+function OxocardUploader(){
+	this.COMPILE_URL = 'http://localhost:1337/compile';
+	this.UPLOAD_URL = 'https://localhost:8992/upload';
+	this.FLASH_COMMAND={
+		"board": "arduino:avr:oxocard",
+		"port": "/dev/ttyUSB0",
+		"filename": "sketch_jul11a.hex",
+		"commandline": "\"{runtime.tools.avrdude.path}/bin/avrdude\" \"-C{runtime.tools.avrdude.path}/etc/avrdude.conf\" {upload.verbose}  -patmega328p -carduino -P{serial.port} -b57600 -D \"-Uflash:w:{build.path}/{build.project_name}.hex:i\" -V", // -V nor reading back, half-time flash
+		"extra": {
+			"auth": {
+				"password": null
+			},
+			"wait_for_upload_port": false,
+			"use_1200bps_touch": false,
+			"network": false,
+			"params_verbose": "-v",
+			"params_quiet": "-q -q",
+			"verbose": false
 		}
+	};
+
+	this.compileAndUploadCurrentWorkspace = function(){
+		var sourceCode = Blockly.Arduino.workspaceToCode(Ardublockly.workspace);
+		this.compileAndUpload(sourceCode);
 	}
 
-	http.send(inoContent);
+	this.compileAndUpload = function(source){
+		this.compile(source, function(response){
+			if('hex' in response){
+				this.upload(response['hex'], function(innerResponse){
+					console.log(innerResponse);
+				});
+			}
+		});
+	}
+
+	this.callbackCompile = function(){};
+	this.callbackUpload = function(){};
+
+	this.compile = function(inoContent, callback){
+		callback = callback || this.callbackCompile;
+		this.httpPostRequest(inoContent, OxocardUploader.COMPILE_URL, callback);
+	};
+
+	this.upload = function(hex, callback){
+		callback = callback || this.callbackUpload;
+		// hacky way to ensure we have a copy of the object
+		var request = JSON.parse(JSON.stringify(OxocardUploader.FLASH_COMMAND));
+		request['hex'] = hex;
+		this.httpPostRequest(request, this.UPLOAD_URL, callback)
+		http.send(JSON.stringify(request));
+	};
+
+	this.httpPostRequest = function(data, url, callback){
+		var http = new XMLHttpRequest();
+
+		http.onreadystatechange = function() {
+			if(http.readyState == 4 && http.status == 200) {
+				if(http.responseText){
+					try{
+						callback(JSON.parse(http.responseText));
+					} catch(e) {
+						callback(http.responseText);
+					}
+				}else{
+					console.log('Something wrong with response in body of: ' + url);
+				}
+			}else if(http.readyState == 4 ){
+				console.log('Something wrong on the backend?! url: ' + url);
+			}else{
+				// ignore for now.
+			}
+		}
+
+		var contentType = 'text/plain';
+
+		if(typeof data === 'object' || typeof data === 'array'){
+			data = JSON.stringify(data);
+			contentType = 'application/json';
+		}
+
+		http.setRequestHeader('Content-type',contentType);
+		http.send(data);
+	}
 }
 
-OxocardUploader.upload = function(hex){
-
-	var http = new XMLHttpRequest();
-	http.open("POST", OxocardUploader.UPLOAD_URL, true);
-
-	http.setRequestHeader("Content-type","application/json");
-
-	http.onreadystatechange = function() {
-		if(http.readyState == 4 && http.status == 200) {
-			console.log("upload request finished.");
-			var response = JSON.parse(http.responseText);
-			console.log(response);
-		}
-	}
-
-	var request = JSON.parse(JSON.stringify(OxocardUploader.FLASH_COMMAND));
-	request['hex'] = hex;
-	http.send(JSON.stringify(request));
-}
+var oxocardUploader = oxocardUploader || new OxocardUploader();

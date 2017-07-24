6542b554cc78897da1e36cd3e35cce45a9edc60f
Tim Luginbuehl <tl@oxon.ch>
2017-07-24 14:01:00 +0200

    bugfixing couple of refactor mistakes.

--- ardublockly/oxocard_uploader.js
+++ ardublockly/oxocard_uploader.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function OxocardUploader(){
-	this.COMPILE_URL = 'http://localhost:1337/compile/arduino';
+	var self = this;
+	this.COMPILE_URL = 'http://compile.oxocard.ch/compile/arduino';
 	this.UPLOAD_URL = 'https://localhost:8992/upload';
 	this.FLASH_COMMAND={
 		"board": "arduino:avr:duo",
@@ -21,15 +22,15 @@ function OxocardUploader(){
 		}
 	};
 
-	this.compileAndUploadCurrentWorkspace = function(){
+	self.compileAndUploadCurrentWorkspace = function(){
 		var sourceCode = Blockly.Arduino.workspaceToCode(Ardublockly.workspace);
 		this.compileAndUpload(sourceCode);
 	}
 
-	this.compileAndUpload = function(source){
-		this.compile(source, function(response){
+	self.compileAndUpload = function(source){
+		self.compile(source, function(response){
 			if('hex' in response){
-				this.upload(response['hex'], function(innerResponse){
+				self.upload(response['hex'], function(innerResponse){
 					console.log(innerResponse);
 				});
 			}
@@ -47,10 +48,9 @@ function OxocardUploader(){
 	this.upload = function(hex, callback){
 		callback = callback || this.callbackUpload;
 		// hacky way to ensure we have a copy of the object
-		var request = JSON.parse(JSON.stringify(OxocardUploader.FLASH_COMMAND));
+		var request = JSON.parse(JSON.stringify(this.FLASH_COMMAND));
 		request['hex'] = hex;
 		this.httpPostRequest(request, this.UPLOAD_URL, callback)
-		http.send(JSON.stringify(request));
 	};
 
 	this.httpPostRequest = function(data, url, callback){
@@ -58,15 +58,17 @@ function OxocardUploader(){
 		http.open("POST", url, true);
 		http.onreadystatechange = function() {
 			if(http.readyState == 4 && http.status == 200) {
-				//if(http.responseText){
-					////try{
-						callback(JSON.parse(http.responseText));
-					////} catch(e) {
-						callback(http.responseText);
-					//}
-				//}else{
-				//	console.log('Something wrong with response in body of: ' + url);
-				//}
+				if(http.responseText){
+					var parsedResponse = http.responseText;
+					try{
+						parsedResponse= JSON.parse(http.responseText);
+					} catch(e) {
+						console.log("Failed to parse json: " + e);
+					}
+					callback(parsedResponse);
+				}else{
+					console.log('Something wrong with response in body of: ' + url);
+				}
 			}else if(http.readyState == 4 ){
 				console.log('Something wrong on the backend?! url: ' + url);
 			}else{
